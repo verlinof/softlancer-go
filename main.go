e909f1c547e000f53b3841fca8f1d26a5c941470
verlinof <ajikoko.zx@gmail.com>
2024-08-26 15:20:52 +0700

    Restructuring System

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "github.com/verlinof/softlancer-go/bootstrap"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/verlinof/softlancer-go/config/app_config"
+	"github.com/verlinof/softlancer-go/config/db_config"
+	"github.com/verlinof/softlancer-go/database"
+	"github.com/verlinof/softlancer-go/routes"
+)
 
 func main() {
-	bootstrap.Bootstrap()
+	//Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
+	//Load config
+	app_config.InitAppConfig()
+	db_config.InitDatabaseConfig()
+
+	//Load database
+	database.ConnectDatabase()
+
+	//Init GIN ENGINE
+	app := gin.Default()
+
+	routes.InitRoute(app)
+
+	app.Run(app_config.PORT)
 }
